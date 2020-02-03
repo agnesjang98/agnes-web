11b9549e297db2bb1cbb156a982fa522a49f953b
Agnes Jang <agnesj@uci.edu>
2020-02-03 00:28:33 -0800

    Updates

---
+++ precache-manifest.9cfffce318504bbdb1bb4f7915f3a80f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08576f3808829afe2689715a5996a0e9",
+    "url": "/agnes-web/index.html"
+  },
+  {
+    "revision": "faff762bc2797ada4193",
+    "url": "/agnes-web/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "821a3974a4d163e58554",
+    "url": "/agnes-web/static/js/2.012dcc16.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/agnes-web/static/js/2.012dcc16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faff762bc2797ada4193",
+    "url": "/agnes-web/static/js/main.ebf00141.chunk.js"
+  },
+  {
+    "revision": "bd55f99ab17ad6c48a28",
+    "url": "/agnes-web/static/js/runtime-main.b7de73b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/agnes-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
